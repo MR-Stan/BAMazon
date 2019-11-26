206fad616cb8799c82d984a3b944a727fcb2ac68
MR-Stan <michaelrstanley87@gmail.com>
2019-11-25 19:03:39 -0600

    completed add to cart function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 // npm inquirer package
 const inquirer = require("inquirer");
 
+let currentItem = {};
+
 let customerMethods = {
     // prompts customer to select a transaction
     chooseTransaction: function () {
@@ -13,7 +15,8 @@ let customerMethods = {
                     'Display Products',
                     'Add to Cart',
                     'Show Items in Cart',
-                    'Check Out'
+                    'Check Out',
+                    'Exit'
                 ]
             }
         ]).then(data => {
@@ -30,6 +33,9 @@ let customerMethods = {
                     break;
                 case "Check Out":
                     checkOut();
+                    break;
+                case "Exit":
+                    process.exit();
                     break;
             }
         });
@@ -58,7 +64,6 @@ function displayProducts() {
                 );
                 console.log("---------------------------------------------------------------------------------------------------")
                 if (i === res.length - 1) {
-                    //connection.end();
                     customerMethods.chooseTransaction();
                 }
             }
@@ -73,7 +78,7 @@ function addCart() {
 
     inquirer.prompt([
         {
-            type: 'number',
+            type: 'number', // need to validate
             name: 'itemid',
             message: 'Enter the ID number of the item you\'d like to purchase: ',
         }
@@ -85,16 +90,51 @@ function addCart() {
             },
             function (err, res) {
                 if (err) throw err;
-                console.log("---------------------------------------------------------------------------------------------------")
-                console.log(
-                    "Item ID: " + res[0].item_id +
-                    " | Name: " + res[0].product_name +
-                    " | Department: " + res[0].department_name +
-                    " | Price: $" + res[0].price +
-                    " | Quantity in Stock: " + res[0].stock_quantity
-                );
-                console.log("---------------------------------------------------------------------------------------------------")
+                else if (res[0].stock_quantity > 0) {
+                    currentItem = res[0];
+                    console.log("---------------------------------------------------------------------------------------------------")
+                    console.log(
+                        "Item ID: " + res[0].item_id +
+                        " | Name: " + res[0].product_name +
+                        " | Department: " + res[0].department_name +
+                        " | Price: $" + res[0].price +
+                        " | Quantity in Stock: " + res[0].stock_quantity
+                    );
+                    console.log("---------------------------------------------------------------------------------------------------")
+                    inquirer.prompt([
+                        {
+                            type: 'list',
+                            name: 'answer',
+                            message: 'You\'ve selected ' + res[0].product_name + '. Is this correct?',
+                            choices: [
+                                'Yes',
+                                'No'
+                            ]
+                        }
+                    ]).then(data => {
+                        if (data.answer === 'Yes') {
+                            inquirer.prompt([
+                                {
+                                    type: 'number', // need to validate
+                                    name: 'quantity',
+                                    message: 'How many would you like to purchase?',
+                                }
+                            ]).then(data => {
+                                checkOrder(data.quantity);
+                            });
+                        }
+                        else {
+                            customerMethods.chooseTransaction();
+                        }
+                    });
+                }
+                else {
+                    console.log("Sorry! " + res[0].product_name + " is currently out of stock. Please try another item")
+                    addCart();
+                }
+
             }
+
         );
         // if stock > 0, prompt the user how many they want to purchse, based on that amount call checkorder, if there is sufficient amount, ask the user to confirm, push or cart and update db
         // checkOrder();
@@ -113,17 +153,33 @@ function checkOut() {
 }
 
 // check the user inputs from howMany to see if the store has enough of the product
-function checkOrder() {
-    // should be greater than user input not 0
-    if (item.quantity > 0) {
-        // update sql database by removing the requested amount
-        // show the customer their total for that item
+function checkOrder(quantity) {
+    if (quantity <= currentItem.stock_quantity) {
+        let total = parseFloat(quantity * currentItem.price);
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'answer',
+                message: 'Are you sure you want to buy ' + quantity + " " + currentItem.product_name + "'s for $" + total + "?",
+                choices: [
+                    'Yes',
+                    'No'
+                ]
+            }
+        ]).then(data => {
+            if (data.answer === 'Yes') {
+                // update sql database by removing the requested amount
+            }
+            else {
+                console.log("No problem. Exiting to menu.")
+                customerMethods.chooseTransaction();
+            }
+
+        });
     }
     else {
-        console.log(`Dear Valued Customer, \nWe sincerely apologize for the inconvenience, 
-            but we're currently out of stock in your selected product. Please select
-            another item.`)
+        console.log("Dear Valued Customer, \nWe sincerely apologize for the inconvenience, but we're currently out of stock in your selected product. Please select another item.");
+        addCart();
     }
 }
-
 module.exports = customerMethods;
