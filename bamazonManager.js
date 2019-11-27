06a39c8544e6d115fdb94ba574925ace6f5568f1
MR-Stan <michaelrstanley87@gmail.com>
2019-11-26 18:21:38 -0600

    manager is complete

--- bamazonManager.js
+++ bamazonManager.js
@@ -100,7 +100,111 @@ function lowInventory() {
 
 // display prompt that will let user add more of any item in store
 function addInventory() {
-    // copy and update purchase function from customer
+    const config = require("./databaseConfig");
+    let connection = config.connection;
+
+    inquirer.prompt([
+        {
+            type: 'number', // need to validate
+            name: 'itemid',
+            message: 'Enter the ID number of the item you\'d like increase stock: ',
+        }
+    ]).then(data => {
+        connection.query(
+            "SELECT * FROM products WHERE item_id =" + data.itemid,
+            {
+                item_id: data.itemid
+            },
+            function (err, res) {
+                if (err) throw err;
+                else if (res[0].stock_quantity > 0) {
+                    currentItem = res[0];
+                    console.log("---------------------------------------------------------------------------------------------------")
+                    console.log(
+                        "Item ID: " + res[0].item_id +
+                        " | Name: " + res[0].product_name +
+                        " | Department: " + res[0].department_name +
+                        " | Price: $" + res[0].price +
+                        " | Quantity in Stock: " + res[0].stock_quantity
+                    );
+                    console.log("---------------------------------------------------------------------------------------------------")
+                    inquirer.prompt([
+                        {
+                            type: 'list',
+                            name: 'answer',
+                            message: 'You\'ve selected ' + res[0].product_name + '. Is this correct?',
+                            choices: [
+                                'Yes',
+                                'No'
+                            ]
+                        }
+                    ]).then(data => {
+                        if (data.answer === 'Yes') {
+                            inquirer.prompt([
+                                {
+                                    type: 'number', // need to validate
+                                    name: 'quantity',
+                                    message: 'How much stock would you like to add?',
+                                }
+                            ]).then(data => {
+                                let quantity = data.quantity;
+                                if (quantity > 0) {
+                                    inquirer.prompt([
+                                        {
+                                            type: 'list',
+                                            name: 'answer',
+                                            message: 'Are you sure you want to add ' + quantity + " " + currentItem.product_name + "'s?",
+                                            choices: [
+                                                'Yes',
+                                                'No'
+                                            ]
+                                        }
+                                    ]).then(data => {
+                                        if (data.answer === 'Yes') {
+                                            let newStock = parseInt(currentItem.stock_quantity + quantity);
+                                            // updates local quantity, may not be needed
+                                            currentItem.stock_quantity = newStock;
+                                            connection.query(
+                                                "UPDATE products SET ? WHERE ?",
+                                                [
+                                                    {
+                                                        stock_quantity: newStock
+                                                    },
+                                                    {
+                                                        item_id: currentItem.item_id
+                                                    }
+                                                ],
+                                                function (err) {
+                                                    if (err) throw err;
+                                                    console.log(quantity + " " + currentItem.product_name + "'s added to cart.");
+                                                    console.log("---------------------------------------");
+                                                    customer.chooseTransaction();
+                                                });
+                                        }
+                                        else {
+                                            console.log("No problem. Exiting to menu.")
+                                            customer.chooseTransaction();
+                                        }
+                                    });
+                                }
+                                else {
+                                    console.log("Please enter a positive quantity");
+                                    addInventory();
+                                }
+                            });
+                        }
+                        else {
+                            customer.chooseTransaction();
+                        }
+                    });
+                }
+                else {
+                    console.log("Sorry! " + res[0].product_name + " is currently out of stock. Please try another item")
+                    addCart();
+                }
+            }
+        );
+    });
 }
 
 // add new product to store
